a0041467af78d0ad2e34cacdd1d51066eac5abdb
Jeremy Lorette <jlorette@2A6.42angouleme.fr>
2024-11-18 17:31:20 +0100

    refacto string

--- src/client.c
+++ src/client.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jlorette <jlorette@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/16 15:18:06 by jlorette          #+#    #+#             */
-/*   Updated: 2024/09/16 16:11:26 by jlorette         ###   ########.fr       */
+/*   Created: 2024/11/16 15:18:06 by jlorette          #+#    #+#             */
+/*   Updated: 2024/11/18 17:25:14 by jlorette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,46 +15,43 @@
 #include "../ft_printf/include/ft_printf.h"
 #include "../libft/libft.h"
 
-void	ascii_to_binary(int pid, char c)
+void    binary_to_ascii(int pid, char c)
 {
-	int	bit;
+    int    bit;
 
-	bit = 0;
-	while (bit < 8)
-	{
-		if ((c & (0x01 << bit)))
-			kill(pid, SIGUSR1);
-		else
-			kill(pid, SIGUSR2);
-		usleep(300);
-		bit++;
-	}
+    bit = 7;
+    while (bit >= 0)
+    {
+        if ((c >> bit) & 1)
+            kill(pid, SIGUSR2);
+        else
+            kill(pid, SIGUSR1);
+        bit--;
+        usleep(300);
+    }
 }
 
-int	main(int argc, char **argv)
+int    main(int argc, char *argv[])
 {
-	int	pid;
-	int	i;
+    int        pid;
+    int        i;
+    char    *str;
 
-	i = 0;
-	if (argc == 3)
-	{
-		pid = ft_atoi(argv[1]);
-		if (pid <= 0)
-		{
-			ft_putstr_fd("Error\n", 2);
-			return (1);
-		}
-		while (argv[2][i] != '\0')
-		{
-			ascii_to_binary(pid, argv[2][i]);
-			i++;
-		}
-	}
-	else
-	{
-		ft_putstr_fd("Error\n", 2);
-		return (1);
-	}
-	return (0);
+    i = 0;
+    if (argc == 3)
+    {
+        pid = ft_atoi(argv[1]);
+        str = argv[2];
+        if (pid <= 0)
+            return (0);
+
+        while (str[i] != '\0')
+        {
+            binary_to_ascii(pid, str[i++]);
+        }
+        binary_to_ascii(pid, str[i]);
+    }
+    else
+        ft_printf("Message format not valide\n");
+    return (0);
 }
--- src/server.c
+++ src/server.c
@@ -6,45 +6,88 @@
 /*   By: jlorette <jlorette@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 14:33:27 by jlorette          #+#    #+#             */
-/*   Updated: 2024/09/17 13:54:59 by jlorette         ###   ########.fr       */
+/*   Updated: 2024/11/18 17:27:51 by jlorette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
+#include <stdlib.h>
 #include "../ft_printf/include/ft_printf.h"
 
-void	binary_to_ascii(int signal)
+char    *ft_strjoin_char(char const *s1, char c)
 {
-	static int	bit;
-	static int	i;
+    int        len_total;
+    int        i;
+    char    *result;
 
-	if (signal == SIGUSR1)
-		i |= (0x01 << bit);
-	bit++;
-	if (bit == 8)
-	{
-		ft_printf("%c", i);
-		bit = 0;
-		i = 0;
-	}
+    if (!s1 || !c)
+        return (NULL);
+    i = 0;
+    len_total = ft_strlen(s1) + 1;
+    result = malloc(sizeof(char) * (len_total + 1));
+    if (result == NULL)
+        return (NULL);
+    while (s1[i])
+    {
+        result[i] = s1[i];
+        i++;
+    }
+    result[i++] = c;
+    result[i] = '\0';
+    free((char *)s1);
+    return (result);
 }
 
-int	main(int argc, char **argv)
+void    handle_completion(char **buffer, unsigned char *current_byte,
+        int *count_bits)
 {
-	int	pid;
+    if (*count_bits > 7)
+    {
+        if (*current_byte == 0)
+        {
+            ft_printf("%s", *buffer);
+            free(*buffer);
+            *buffer = NULL;
+        }
+        else
+            *buffer = ft_strjoin_char(*buffer, *current_byte);
+        *count_bits = 0;
+        *current_byte = 0;
+    }
+}
 
-	(void)argv;
-	if (argc != 1)
-	{
-		ft_printf("Error\n", 2);
-		return (1);
-	}
-	pid = getpid();
-	ft_printf("Server PID: [%d]\n", pid);
-	while (argc == 1)
-	{
-		signal(SIGUSR1, binary_to_ascii);
-		signal(SIGUSR2, binary_to_ascii);
-		pause();
-	}
+void    sig_reception(int sig)
+{
+    static char                *buffer;
+    static int                count_bits;
+    static unsigned char    current_byte;
+
+    if (!buffer)
+    {
+        buffer = malloc(1);
+        if (!buffer)
+            return ;
+        buffer[0] = 0;
+    }
+    if (sig == SIGUSR1)
+    {
+        current_byte = (current_byte << 1) | 0;
+        count_bits++;
+    }
+    else if (sig == SIGUSR2)
+    {
+        current_byte = (current_byte << 1) | 1;
+        count_bits++;
+    }
+    handle_completion(&buffer, &current_byte, &count_bits);
+}
+
+int    main(void)
+{
+    ft_printf("PID [%d]\n", getpid());
+    signal(SIGUSR1, sig_reception);
+    signal(SIGUSR2, sig_reception);
+    while (1)
+        pause();
+    return (0);
 }
